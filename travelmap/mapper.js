f7c6488fc21dfeec734def8d174b0a4f8ebdc2fa
James McGlone <jim.mcglone@gmail.com>
2017-10-11 02:44:25 -0400

    add travel map contents

---
+++ travelmap/mapper.js
@@ -0,0 +1,29 @@
+// psuedo coding!!! :)
+
+
+// process
+// 1. thinking programmatically
+// 2. extending attention span
+// 3. building something fun/that i enjoy
+// 4. develop skills that can be applied to other maps/d3.js projects
+
+
+// Objectives:
+// 1. [C] display map 													
+// 2. [C] identify countries visited 									
+// 3. [C] color according to length of time visited 					
+// 4. [C] label: country name, days stayed (unless US or China)
+// 5. [C] cities visited 											
+// 6. [N] labeling for cities: name, province (if applicable), country
+// 6.5 Toggle city layer 
+// 7. [N] styling ???
+// 8. zoom in/zoom out 
+// 9. legend: list of countries, days stayed
+// 10. legend: bar for range of days stayed: 1, 2, 4, 8, 16, 
+// 11. timeline - when visited
+
+
+// Thanks to:
+// http://bl.ocks.org/mattparrilla/5724610
+// https://bost.ocks.org/mike/map/
+// http://bl.ocks.org/KoGor/5685876
