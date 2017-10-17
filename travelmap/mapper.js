e10677c42b1da9efd0b645c8f5bf41859cc13002
James McGlone <jim.mcglone@gmail.com>
2017-10-16 22:42:12 -0700

    travelmap ready

--- travelmap/mapper.js
+++ travelmap/mapper.js
@@ -14,9 +14,10 @@
 // 3. [C] color according to length of time visited 			
 // 4. [C] mouseover label: country name, duration: # days || # years *	
 // 5. [C] cities visited error in console + bermuda problem										
-// 6. [] Toggle city layer *
-// 7. [] mouseover labeling for cities: name, province (if applicable), country *
+// 6. [C] Toggle city layer *
+// 7. [C] mouseover labeling for cities: name, province (if applicable), country *
 // 8. [] legend: list of countries visited  w/ colored square + bar for range of duration
+// 9. [C] Zoom in/out
 // 9. [] wrapper for .js file to div container
 
 // *** Write README ***
@@ -25,7 +26,6 @@
 // 1. Add to new site
 // 2. Region details:
 // a. US and China Details - use this tutorial as guide: http://www.tnoda.com/blog/2013-12-07
-// b. zoom in/out
 // 3. timeline of duration by country
 // 4. flight/train info - 
 // a. travel type from point a to point b
